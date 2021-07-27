e35d8ff964e1bc1257192bed9ede99b6b19067f9
Breyson Mosquera <breysonjoe107@gmail.com>
2021-07-26 23:30:10 -0500

    Primer Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('content')
+<div class="container">
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Bienvenido') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Aqui empezaran las configuraciones') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
